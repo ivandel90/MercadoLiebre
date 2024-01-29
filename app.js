b7c686e9615452febaccab8193065b2b6a87a173
ivandel90 <ivandel1990@gmail.com>
2024-01-29 13:32:38 -0300

    proyecto terminado

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ const path = require("path")
 app.use(express.static('public'))
 app.get("/",(req,res) => { 
 res.sendFile(path.join(__dirname,"views/home.html"))})
-app.listen(port,() => console.log(`http://localhost:${port}`))  
+
+
+
+
+
+
+app.listen(port,() => console.log(`http://localhost:${port}`))  //levanta servidor
+
 
